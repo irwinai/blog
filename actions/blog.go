80f68119f338c74e990d81ab9677d1e7ee391932
awl <415586662@qq.com>
2014-06-21 23:36:13 +0800

    add blog detail page

--- actions/blog.go
+++ actions/blog.go
@@ -5,9 +5,14 @@ import "github.com/lunny/xweb"
 type BlogAction struct {
 	BaseAction
 
-	list xweb.Mapper
+	list   xweb.Mapper
+	detail xweb.Mapper
 }
 
 func (c *BlogAction) List() error {
 	return c.Render("list.html")
+}
+
+func (c *BlogAction) Detail() error {
+	return c.Render("blog.html")
 }
