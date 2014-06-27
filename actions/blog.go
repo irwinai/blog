202247cb493681268df53b4998102f0e32c547ac
irwinai <415586662@qq.com>
2014-06-27 18:29:00 +0800

    modify add blog page

--- actions/blog.go
+++ actions/blog.go
@@ -14,14 +14,39 @@ type BlogAction struct {
 	archive xweb.Mapper
 	add     xweb.Mapper
 
-	Blog Blog
+	Blog   Blog
+	Status string
+	Cat    []int
+	Tags   string
 }
 
 func (c *BlogAction) Add() {
 	if c.Method() == "GET" {
 		c.Render("manager/add-blog.html")
-		fmt.Println(111)
 	} else if c.Method() == "POST" {
+		if c.Status == "on" {
+			c.Blog.Status = 1
+		} else {
+			c.Blog.Status = 0
+		}
+		_, err := c.Orm.Insert(c.Blog)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if c.Cat != nil {
+			list := make([]*BlogCategory, 10)
+			for _, value := range c.Cat {
+				bc := new(BlogCategory)
+				bc.BlogId = c.Blog.Id
+				bc.CategoryId = value
+				list = append(list, bc)
+			}
+			c.Orm.Insert(list)
+		}
+		fmt.Print("status=", c.Status)
+		fmt.Println("cat = ", c.Cat)
+		fmt.Println("tags = ", c.Tags)
 		//c.Orm.Insert(Blog)
 	}
 }
