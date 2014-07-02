ef37050fa46e0a4e328bb9d68a2913409982cc6a
irwinai <415586662@qq.com>
2014-07-02 18:00:15 +0800

    add manager list blog page

--- actions/blog.go
+++ actions/blog.go
@@ -2,8 +2,6 @@ package actions
 
 import (
 	"fmt"
-	"strings"
-	"time"
 
 	"github.com/lunny/xweb"
 )
@@ -14,15 +12,16 @@ type BlogAction struct {
 	list    xweb.Mapper
 	detail  xweb.Mapper
 	archive xweb.Mapper
-	add     xweb.Mapper
 	paging  xweb.Mapper
-
-	Blog   Blog
-	Status string
-	Cat    []int
-	Tags   string
+	like    xweb.Mapper
 }
 
+//赞功能
+func (c *BlogAction) Like() {
+
+}
+
+//博客详情页左右翻页功能
 func (c *BlogAction) Paging() {
 	key := c.GetString("type")
 	id, _ := c.GetInt("id")
@@ -44,75 +43,6 @@ func (c *BlogAction) Paging() {
 		c.Go(fmt.Sprintf("detail?id=%v", mid))
 	} else {
 		c.Go(fmt.Sprintf("detail?id=%v", id))
-	}
-}
-
-func (c *BlogAction) Add() {
-	if c.Method() == "GET" {
-		c.Render("manager/add-blog.html")
-	} else if c.Method() == "POST" {
-		session := c.Orm.NewSession()
-		defer session.Close()
-		defer session.Commit()
-		session.Begin()
-		if c.Status == "on" {
-			c.Blog.Status = 1
-		} else {
-			c.Blog.Status = 0
-		}
-		c.Blog.CreateTime = time.Now()
-		c.Blog.UpdateTime = time.Now()
-		//保存博客
-		_, err := session.Insert(&c.Blog)
-		if err != nil {
-			fmt.Println(err)
-			session.Rollback()
-			return
-		}
-		//保存博客分类关联
-		if c.Cat != nil {
-			list := make([]*BlogCategory, len(c.Cat))
-			for key, value := range c.Cat {
-				bc := new(BlogCategory)
-				bc.BlogId = c.Blog.Id
-				bc.CategoryId = value
-				list[key] = bc
-			}
-			session.Insert(&list)
-		}
-		//保存博客标签关联
-		if c.Tags != "" {
-			tags := strings.Split(c.Tags, ",")
-			tagList := make([]*BlogTag, len(tags))
-			for key, value := range tags {
-				tag := new(Tag)
-				has, err := session.Where("name=? and status=?", value, 1).Get(tag)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				//如果该标签已经存在
-				bt := new(BlogTag)
-				bt.BlogId = c.Blog.Id
-				if has {
-					bt.TagId = tag.Id
-				} else {
-					newTag := new(Tag)
-					newTag.Name = value
-					newTag.Status = 1
-					_, err := session.Insert(newTag)
-					if err != nil {
-						fmt.Println(err)
-						session.Rollback()
-						return
-					}
-					bt.TagId = newTag.Id
-				}
-				tagList[key] = bt
-			}
-			session.Insert(&tagList)
-		}
-		fmt.Println("插入成功======================")
 	}
 }
 
--- actions/manager.go
+++ actions/manager.go
@@ -2,6 +2,8 @@ package actions
 
 import (
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/lunny/xweb"
 )
@@ -11,8 +13,13 @@ type ManagerAction struct {
 
 	home  xweb.Mapper `xweb:"/"`
 	blogs xweb.Mapper `xweb:"/blog/list"`
+	add   xweb.Mapper `xweb:"/blog/add"`
 
-	User User
+	User   User
+	Blog   Blog
+	Status string
+	Cat    []int
+	Tags   string
 }
 
 func (m *ManagerAction) Home() {
@@ -20,13 +27,141 @@ func (m *ManagerAction) Home() {
 }
 
 func (m *ManagerAction) Blogs() {
-	blogs := make([]*Blog, 0)
+	blogs := make([]Blog, 0)
 	err := m.Orm.Where("status=?", 1).Find(&blogs)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	m.Render("blog-list.html", &xweb.T{
-		"blogs": blogs,
-	})
+	if len(blogs) > 0 {
+		result := make([]map[string]interface{}, len(blogs))
+		for key, blog := range blogs {
+			mp := make(map[string]interface{})
+			mp["id"] = blog.Id
+			mp["title"] = blog.Title
+			mp["time"] = blog.CreateTime
+			mp["status"] = blog.Status
+			mp["commentCount"] = blog.CommentCount
+			mp["views"] = blog.Views
+			mp["like"] = blog.Like
+			mp["createTime"] = blog.CreateTime
+			//根据ID查询用户
+			user := new(User)
+			_, err = m.Orm.Id(blog.UserId).Get(user)
+			if err != nil {
+				return
+			}
+			if user != nil {
+				mp["username"] = user.Username
+			}
+			//查询博客分类关联
+			bs := make([]BlogCategory, 0)
+			err = m.Orm.Where("blog_id=?", blog.Id).Find(&bs)
+			if err != nil {
+				return
+			}
+			if len(bs) > 0 {
+				categories := make([]string, len(bs))
+				for key2, value := range bs {
+					category := new(Category)
+					_, err = m.Orm.Id(value.CategoryId).Get(category)
+					if err != nil {
+						return
+					}
+					categories[key2] = category.Name
+				}
+				mp["categories"] = categories
+			}
+			//查询标签
+			bt := make([]BlogTag, 0)
+			err = m.Orm.Where("blog_id=?", blog.Id).Find(&bt)
+			if err != nil {
+				return
+			}
+			if len(bt) > 0 {
+				tags := make([]string, len(bt))
+				for key3, value := range bt {
+					tag := new(Tag)
+					m.Orm.Id(value.TagId).Get(tag)
+					tags[key3] = tag.Name
+				}
+				mp["tags"] = tags
+			}
+			result[key] = mp
+		}
+		m.Render("manager/blog-list.html", &xweb.T{
+			"result": result,
+		})
+	}
+}
+
+func (c *ManagerAction) Add() error {
+	if c.Method() == "GET" {
+		c.Render("manager/add-blog.html")
+	} else if c.Method() == "POST" {
+		session := c.Orm.NewSession()
+		defer session.Close()
+		defer session.Commit()
+		session.Begin()
+		if c.Status == "on" {
+			c.Blog.Status = 1
+		} else {
+			c.Blog.Status = 0
+		}
+		c.Blog.CreateTime = time.Now()
+		c.Blog.UpdateTime = time.Now()
+		//保存博客
+		_, err := session.Insert(&c.Blog)
+		if err != nil {
+			fmt.Println(err)
+			session.Rollback()
+			return err
+		}
+		//保存博客分类关联
+		if c.Cat != nil {
+			list := make([]*BlogCategory, len(c.Cat))
+			for key, value := range c.Cat {
+				bc := new(BlogCategory)
+				bc.BlogId = c.Blog.Id
+				bc.CategoryId = value
+				list[key] = bc
+			}
+			session.Insert(&list)
+		}
+		//保存博客标签关联
+		if c.Tags != "" {
+			tags := strings.Split(c.Tags, ",")
+			tagList := make([]*BlogTag, len(tags))
+			for key, value := range tags {
+				tag := new(Tag)
+				has, err := session.Where("name=? and status=?", value, 1).Get(tag)
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				//如果该标签已经存在
+				bt := new(BlogTag)
+				bt.BlogId = c.Blog.Id
+				if has {
+					bt.TagId = tag.Id
+				} else {
+					newTag := new(Tag)
+					newTag.Name = value
+					newTag.Status = 1
+					_, err := session.Insert(newTag)
+					if err != nil {
+						fmt.Println(err)
+						session.Rollback()
+						return err
+					}
+					bt.TagId = newTag.Id
+				}
+				tagList[key] = bt
+			}
+			session.Insert(&tagList)
+		}
+		fmt.Println("插入成功======================")
+		c.Go("blogs")
+	}
+	return xweb.NotSupported()
 }
