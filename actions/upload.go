f1613af497f834b93484bbadeeabe215e3f9025d
irwinai <415586662@qq.com>
2014-07-07 18:13:37 +0800

    modify picture upload func

--- actions/upload.go
+++ actions/upload.go
@@ -2,7 +2,11 @@ package actions
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"strings"
 
+	"code.google.com/p/go-uuid/uuid"
 	"github.com/lunny/xweb"
 )
 
@@ -12,24 +16,26 @@ type UploadAction struct {
 	home xweb.Mapper `xweb:"/"`
 }
 
+//处理文件上传
 func (c *UploadAction) Home() {
-	fmt.Println("=======================================")
-	file, head, err := c.GetFile("filedata")
+	path := c.cfg.Map()["filepath"]
+	fmt.Println("===============file path========================", path)
+	fmt.Println("===============file size=======================", c.Request.ContentLength/1024/1024)
+
+	h := c.Header("Content-Disposition")
+	array := strings.Split(h, ";")
+	filename := array[2][11 : len(array[2])-1]
+	fmt.Println("===============file ext=======================", filename)
+
+	random := uuid.NewUUID()
+	os.MkdirAll(path, 0666)
+	f, err := os.OpenFile(fmt.Sprintf("%v\\%v.jpg", path, random), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
-	//sroot := c.cfg.Map()["filepath"]
-	fmt.Println("========================================================")
-	fmt.Println(head.Filename, "----------------")
-	fmt.Println(file, "----------------")
+	defer f.Close()
+
+	io.Copy(f, c.Request.Body)
+	io.Copy(c.ResponseWriter, f)
 	c.ServeJson(map[string]interface{}{"err": "", "msg": "200906030521128703.gif"})
-	// f, err := os.OpenFile(, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-	// if err != nil {
-	// 	return err
-	// }
-	// defer f.Close()
-	// _, err = io.Copy(f, file)
-	// return err
-	//	return xweb.NotSupported()
 }
---
+++ test.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "123456"
+	fmt.Println(s[1:2])
+}
